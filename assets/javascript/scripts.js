da5ded5876cfa000bff4ce7c446cb36dfa2dabec
dnl092 <drogalsky14@my.whitworth.edu>
2019-06-03 10:05:55 -0700

    added ids and object for the selectable characters

---
+++ assets/javascript/scripts.js
@@ -0,0 +1,33 @@
+var background = {
+
+};
+
+var aaylaSecura = {
+    baseHP: 50,
+    baseAttack: 5,
+    currentHP: 30,
+    currentAttack: 5
+    // 5,10,15,20,25,30,35,40,45,50
+};
+
+var yoda = {
+    baseHP: 30,
+    baseAttack: 6,
+    currentHP: 30,
+    currentAttack: 6
+    // 6,12,18,24,30,36,42,48,54,60
+};
+
+var maceWindu = {
+    baseHP: 70,
+    baseAttack: 4,
+    currentHP: 70,
+    currentAttack: 4
+    // 4,8,12,16,20,24,28,32,36,40
+};
+
+var darthMaul = {
+    baseHP: 10
+    counterAttack: 1
+    currentHP:
+}
