83227f4530cd1c60f6f223a292052d46313a3e27
dnl092 <drogalsky14@my.whitworth.edu>
2019-06-04 11:37:54 -0700

    added basic css and put images in html and put heroes and villians in arrays

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -10,6 +10,12 @@ let jediYoda = $('Yoda');
 var background = {
     hero: "",
     villian: "",
+    characterSelect(chosenHero) {
+        this.hero = chosenHero;
+    },
+    enemySelect(chosenTarget) {
+        this.villian = chosenTarget;
+    }
     fight() {
         villian.currentHP -= hero.currentAttack;
         hero.currentHP -= villian.counterAttack;
@@ -25,58 +31,73 @@ var background = {
         //TODO:reset everything and make the character select reapear
     }
 };
+var heroArray = [
+    aaylaSecura = {
+        value: 'aaylaSecura',
+        baseHP: 50,
+        baseAttack: 5,
+        currentHP: 30,
+        currentAttack: 5
+        // 5,10,15,20,25,30,35,40,45,50
+    },
 
-var aaylaSecura = {
-    baseHP: 50,
-    baseAttack: 5,
-    currentHP: 30,
-    currentAttack: 5
-    // 5,10,15,20,25,30,35,40,45,50
-};
+    yoda = {
+        value: 'yoda',
+        baseHP: 30,
+        baseAttack: 6,
+        currentHP: 30,
+        currentAttack: 6
+        // 6,12,18,24,30,36,42,48,54,60
+    },
 
-var yoda = {
-    baseHP: 30,
-    baseAttack: 6,
-    currentHP: 30,
-    currentAttack: 6
-    // 6,12,18,24,30,36,42,48,54,60
-};
+    maceWindu = {
+        value: 'maceWindu',
+        baseHP: 70,
+        baseAttack: 4,
+        currentHP: 70,
+        currentAttack: 4
+        // 4,8,12,16,20,24,28,32,36,40
+    }
+];
 
-var maceWindu = {
-    baseHP: 70,
-    baseAttack: 4,
-    currentHP: 70,
-    currentAttack: 4
-    // 4,8,12,16,20,24,28,32,36,40
-};
+var villianArray[
+    darthMaul = {
+        value: 'darthMaul',
+        baseHP: 10,
+        counterAttack: 1,
+        currentHP: 10
+    },
 
-var darthMaul = {
-    baseHP: 10,
-    counterAttack: 1,
-    currentHP: 10
-};
+    countDooku = {
+        value: 'countDooku',
+        baseHP: 30,
+        counterAttack: 2,
+        currentHP: 20
+    },
 
-var countDooku = {
-    baseHP: 30,
-    counterAttack: 2,
-    currentHP: 20
+    generalGrievous = {
+        value: 'generalGrievous',
+        baseHP: 60,
+        counterAttack: 4,
+        currentHP: 20
+    },
+
+    darthVader = {
+        value: 'darthVader',
+        baseHP: 150,
+        counterAttack: 3,
+        currentHP: 50
+    },
+
+    palpatine = {
+        value: 'palpatine',
+        baseHP: 100,
+        counterAttack: 6,
+        currentHP: 30
+    }
+]
+
+
+$(document).ready(function() {
+    
 }
-
-var generalGrievous = {
-    baseHP: 60,
-    counterAttack: 4,
-    currentHP: 20
-}
-
-var darthVader = {
-    baseHP: 150,
-    counterAttack: 3,
-    currentHP: 50
-}
-
-var palpatine = {
-    baseHP: 100,
-    counterAttack: 6,
-    currentHP: 30
-}
-
